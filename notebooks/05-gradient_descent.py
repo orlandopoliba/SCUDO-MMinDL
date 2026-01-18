00ff3eaf683e2a570038f2ccac3cd15b1baf91d2
Gianluca Orlando <79985574+orlandopoliba@users.noreply.github.com>
2026-01-18 15:34:38 +0100

    added placeholder files for all notebooks

---
+++ notebooks/05-gradient_descent.py
@@ -0,0 +1,9 @@
+import marimo
+
+__generated_with = "0.19.4"
+app = marimo.App(
+    width="medium",
+    app_title="Gradient Descent",
+    css_file="style.css",
+    auto_download=["html"],
+)
---
+++ notebooks/09-regression_bike_data.py
@@ -0,0 +1,9 @@
+import marimo
+
+__generated_with = "0.19.4"
+app = marimo.App(
+    width="medium",
+    app_title="Regression Bike Data",
+    css_file="style.css",
+    auto_download=["html"],
+)
